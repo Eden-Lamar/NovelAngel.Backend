39150f2c2e264d6cee9c9deb8a4f3ff739908a6a
Eden-Lamr <emmanuelmomodu3@gmail.com>
2024-09-28 13:32:16 +0100

    configured multer and added it's middleware to /profile

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const { registerUser, loginUser, getProfile, updateProfile, getUserBookmarks, getReadingHistory } = require('../../controllers/userController');
 const { protect } = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
+const storage = multer.memoryStorage()
+const upload = multer({
+	storage,
+	limits: { fileSize: 3 * 1024 * 1024 }, // 3MB file size limit
+});
+
 
 // Public routes
 router.post('/register', registerUser); // Register user
@@ -10,7 +17,7 @@ router.post('/login', loginUser);       // Login user
 
 // Protected routes (requires login)
 router.get('/profile', protect, getProfile);       // Get user profile
-router.put('/profile', protect, updateProfile);    // Update user profile
+router.put('/profile', protect, upload.single('avatar'), updateProfile);    // Update user profile
 router.get('/bookmarks', protect, getUserBookmarks);    // Get the list of bookmarked books by the logged-in user
 router.get('/history', protect, getReadingHistory);    // Get a user's reading history
 
