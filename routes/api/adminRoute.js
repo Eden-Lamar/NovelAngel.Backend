43db07bdf51bc05cd5030eaddd469c21924b68a5
Eden-Lamr <emmanuelmomodu3@gmail.com>
2024-09-28 13:32:55 +0100

    configured multer and added it's middleware to /book

--- routes/api/adminRoute.js
+++ routes/api/adminRoute.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const multer = require('multer');
 const { addBook, updateBook, deleteBook, addChapter, updateChapter, deleteChapter } = require('../../controllers/adminController');
 const { protect, admin } = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
+const storage = multer.memoryStorage()
+const upload = multer({
+	storage,
+	limits: { fileSize: 3 * 1024 * 1024 }, // 3MB file size limit
+});
+
 
 // Book Routes
-router.post('/books', protect, admin, addBook);             // Create new book
+router.post('/books', protect, admin, upload.single('bookImage'), addBook);             // Create new book
 router.put('/books/:bookId', protect, admin, updateBook);   // Update a book
 router.delete('/books/:bookId', protect, admin, deleteBook); // Delete a book
 
