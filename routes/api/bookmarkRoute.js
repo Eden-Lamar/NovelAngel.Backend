42b553ff7ddc405c895627199efefa404d2f864c
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-09-23 11:05:57 +0100

    included route for getBookmarkStatus

--- routes/api/bookmarkRoute.js
+++ routes/api/bookmarkRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { toggleBookmark } = require('../../controllers/bookmarkController');
+const { toggleBookmark, getBookmarkStatus } = require('../../controllers/bookmarkController');
 const { protect } = require('../../middlewares/authMiddleware');
 
 // Toggle bookmark on a book
 router.post('/books/:bookId/toggle-bookmark', protect, toggleBookmark);
+
+// Check if a user has bookmarked a book
+router.get('/books/:bookId/bookmark-status', protect, getBookmarkStatus);
 
 module.exports = router;
