63f6cb833d7db862d66a90ddefa70c3a35053640
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-09-23 11:06:45 +0100

    included route for getLikeStatus

--- routes/api/likeRoute.js
+++ routes/api/likeRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { toggleLike } = require('../../controllers/likeController');
+const { toggleLike, getLikeStatus } = require('../../controllers/likeController');
 const { protect } = require('../../middlewares/authMiddleware');
 
 // Toggle like on a book
 router.post('/books/:bookId/toggle-like', protect, toggleLike);
+
+// Get like status for a book
+router.get('/books/:bookId/like-status', protect, getLikeStatus);
 
 module.exports = router;
