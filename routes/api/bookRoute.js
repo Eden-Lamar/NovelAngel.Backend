7b93ffcb09947844f8f6ac727cd2ce45861a8629
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-10-06 10:24:40 +0100

    Added the rotue for unlockChapter endpoint

--- routes/api/bookRoute.js
+++ routes/api/bookRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { searchBooks, getBookById, getChapterById, getNewBooks, getLatestUpdatedBooks, getTrendingBooks, getBookRecommendations, getBookComments, getBookWithComments, getAllBooks } = require('../../controllers/bookController');
-const { optionalAuthMiddleware } = require("../../middlewares/authMiddleware")
+const { searchBooks, getBookById, getChapterById, unlockChapter, getNewBooks, getLatestUpdatedBooks, getTrendingBooks, getBookRecommendations, getBookComments, getBookWithComments, getAllBooks } = require('../../controllers/bookController');
+const { protect, optionalAuthMiddleware } = require("../../middlewares/authMiddleware")
 
 const router = express.Router();
 
@@ -15,6 +15,8 @@ router.get('/books/:id', getBookById); // Get details of a specific book by ID
 router.get('/books/:bookId/comments', getBookComments); // Get comments for a specific book by ID
 router.get('/books/:id/comments/together', getBookWithComments); // Get comments for a specific book by ID
 router.get('/books/:bookId/chapters/:chapterId', optionalAuthMiddleware, getChapterById); // Get details of a specific chapter by ID within a specific book
+router.post('/books/:bookId/chapters/:chapterId/unlock', protect, unlockChapter); // Unlock a locked chapter using coins
+
 router.get('/books/', getAllBooks); // Get all books with pagination 
 
 module.exports = router;
