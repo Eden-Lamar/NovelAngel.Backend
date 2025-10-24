f6437a2b01150e8062b778ccae6fe85a6b9fa974
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-10-24 15:17:29 +0100

    feat: add getContinueReading to user routes for retrieving user's reading progress

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const { registerUser, loginUser, getProfile, updateProfile, getUserBookmarks, getReadingHistory } = require('../../controllers/userController');
+const { registerUser, loginUser, getProfile, updateProfile, getUserBookmarks, getReadingHistory, getContinueReading } = require('../../controllers/userController');
 const { protect } = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -20,5 +20,6 @@ router.get('/profile', protect, getProfile);       // Get user profile
 router.put('/profile', protect, upload.single('avatar'), updateProfile);    // Update user profile
 router.get('/bookmarks', protect, getUserBookmarks);    // Get the list of bookmarked books by the logged-in user
 router.get('/history', protect, getReadingHistory);    // Get a user's reading history
+router.get('/continue-reading', protect, getContinueReading);    // Get a user list of book and chapters they read last
 
 module.exports = router;
