5e68c794476905a59d2e4c9864c04de0a0106062
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-10-30 15:21:16 +0100

    Created this passport google 0Auth config file to work with my app and DB.

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/User.js";
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: process.env.GOOGLE_CALLBACK_URL,
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			try {
+				const email = profile.emails?.[0]?.value;
+				if (!email) return done(null, false, { message: "No email from Google" });
+
+				let user = await User.findOne({ email });
+
+				// If user doesn’t exist, create one (without password)
+				if (!user) {
+					user = await User.create({
+						username: profile.displayName.replaceAll(" ", "_"),
+						email,
+						avatar: profile.photos?.[0]?.value,
+						googleId: profile.id,
+					});
+				}
+				// Passport passes this object into req.user
+				done(null, user);
+			} catch (err) {
+				done(err, null);
+			}
+		}
+	)
+);
+
+export default passport;
