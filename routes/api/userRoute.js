08c3ce5784a60eecf14cd0884c07e91c9e9f6787
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-10-30 15:19:47 +0100

    Added route to login and resiter with google and a callbackroute for google to call when its done

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -2,14 +2,33 @@ const express = require('express');
 const multer = require('multer');
 const { registerUser, loginUser, getProfile, updateProfile, getUserBookmarks, getReadingHistory, getContinueReading } = require('../../controllers/userController');
 const { protect } = require('../../middlewares/authMiddleware');
-
+const { generateToken } = require('../../utils/helpFunction');
+const passport = require('passport');
 const router = express.Router();
 const storage = multer.memoryStorage()
 const upload = multer({
 	storage,
 	limits: { fileSize: 3 * 1024 * 1024 }, // 3MB file size limit
 });
+require('../../config/passport');  // make sure passport is initialized
 
+// Google OAuth route
+router.get(
+	"/auth/google",
+	passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+
+// Google callback
+router.get(
+	"/auth/google/callback",
+	passport.authenticate("google", { session: false }),
+	(req, res) => {
+		const token = generateToken(req.user._id);
+		// Redirect to frontend with JWT
+		res.redirect(`http://localhost:3002/login?token=${token}`);
+	}
+);
 
 // Public routes
 router.post('/register', registerUser); // Register user
