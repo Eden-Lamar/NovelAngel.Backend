0cb987ba9b7b5ff1c1fee50c42b582f45130b93c
Eden-Lamar <emmanuelmomodu3@gmail.com>
2026-07-26 15:09:31 +0100

    Refactor book route identifiers for consistency and clarity

--- routes/api/bookRoute.js
+++ routes/api/bookRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { searchBooks, getBookById, getChapterById, unlockChapter, getNewBooks, getLatestUpdatedBooks, getTrendingBooks, getBookRecommendations, getBookComments, getBookWithComments, getAllBooks, toggleAutoUnlock } = require('../../controllers/bookController');
+const { searchBooks, getBookBySlug, getChapterBySlugAndNumber, unlockChapter, getNewBooks, getLatestUpdatedBooks, getTrendingBooks, getBookRecommendations, getBookComments, getBookWithComments, getAllBooks, toggleAutoUnlock } = require('../../controllers/bookController');
 const { protect, optionalAuthMiddleware } = require("../../middlewares/authMiddleware")
 
 const router = express.Router();
@@ -11,10 +11,10 @@ router.get('/books/latest-updates', getLatestUpdatedBooks); // Get latest update
 router.get('/books/recommendations', optionalAuthMiddleware, getBookRecommendations); //Get random book recommendations
 
 
-router.get('/books/:id', getBookById); // Get details of a specific book by ID
+router.get('/books/:identifier', getBookBySlug); // Get details of a specific book by ID
 router.get('/books/:bookId/comments', getBookComments); // Get comments for a specific book by ID
 router.get('/books/:id/comments/together', getBookWithComments); // Get comments for a specific book by ID
-router.get('/books/:bookId/chapters/:chapterId', optionalAuthMiddleware, getChapterById); // Get details of a specific chapter by ID within a specific book
+router.get('/books/:identifier/chapters/:chapterNo', optionalAuthMiddleware, getChapterBySlugAndNumber); // Get details of a specific chapter by ID within a specific book
 router.post('/books/:bookId/chapters/:chapterId/unlock', protect, unlockChapter); // Unlock a locked chapter using coins
 
 router.get('/books/', getAllBooks); // Get all books with pagination 
