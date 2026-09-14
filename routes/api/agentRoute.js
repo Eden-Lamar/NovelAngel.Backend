f1c9bc2b4526166fca4d64c33c98c64c1f3329f1
Eden-Lamar <emmanuelmomodu3@gmail.com>
2026-09-14 17:25:34 +0100

    Add abort translation route to agent API

--- routes/api/agentRoute.js
+++ routes/api/agentRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { previewTranslation, publishChapter } = require('../../controllers/agentController');
+const { previewTranslation, publishChapter, abortTranslation } = require('../../controllers/agentController');
 const { protect, admin } = require('../../middlewares/authMiddleware');
 const router = express.Router();
 
 // Agent Routes
 router.post('/preview', protect, admin, previewTranslation);
 router.post('/publish', protect, admin, publishChapter);
+
+router.post('/abort', protect, admin, abortTranslation);
 
 module.exports = router;
